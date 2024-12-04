16c5a71ab6cad72ca0129bb197d11d665e47ef47
Hervoy Ancacura B <hervoy.ancacura@gmail.com>
2024-12-03 23:32:13 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'hola-mundo';
+  title = 'hola-mundo desde FireBase Hosting ';
 }
