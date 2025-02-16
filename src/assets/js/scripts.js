facebb50472999c7398af2691c6eb596f162387d
Hervoy Ancacura B <hervoy.ancacura@gmail.com>
2025-02-16 00:27:16 -0300

    Add Profile_160x160.png and remove theme-2.css file
    Replaced the outdated theme-2.css with a new profile image. This update ensures consistent styling and removes unused CSS dependencies.

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const textElement = document.querySelector(".bio span");
   const text = "Hola, mi nombre es Hervoy Ancacura. Desarrollador Backend Java, también me estoy aventurando en FrontEnd con Angular. Solo te puedo adelantar eso, si quieres saber mas de mi y de lo que hago, visita la seccion \"About me\".";
 
-  const velocidadDeEscritura = 50; // Milisegundos por carácter
+  const velocidadDeEscritura = 25; // Milisegundos por caracter
   const duracion = text.length * velocidadDeEscritura / 1000;
 
   const timeline = gsap.timeline();
